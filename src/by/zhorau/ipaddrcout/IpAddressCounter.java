5a441837f37bde593df8509dab900e4aa874ba0a
Ivan <ivanzhuraulievich@gmail.com>
2024-06-26 21:56:34 +0400

    Add ip address string to integer converting

--- src/by/zhorau/ipaddrcout/IpAddressCounter.java
+++ src/by/zhorau/ipaddrcout/IpAddressCounter.java
@@ -1,17 +1,40 @@
 package by.zhorau.ipaddrcout;
 
+import java.nio.ByteBuffer;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class IpAddressCounter {
-    private Iterator<String> addressIterator;
-
+    private final Iterator<String> addressIterator;
+    private Set<Integer> set = new HashSet<>();
     public IpAddressCounter(Iterator<String> addressIterator){
         this.addressIterator = addressIterator;
     }
 
-    public void startCounting(){}
+    public void startCounting(){
+        while(addressIterator.hasNext()){
+            String address = addressIterator.next();
+            byte[] addressArray = convertAddressToByteArray(address);
+            int addressInt = ByteBuffer.wrap(addressArray).getInt();
+            System.out.println(addressInt);
+            //bTree.add(addressInt);
+            set.add(addressInt);
+        }
+    }
+
+    private byte[] convertAddressToByteArray(String address) {
+        byte[] array = new byte[4];
+        int index = 0;
+        for(String part : address.split("\\.")){
+            array[index] = (byte)(Integer.parseInt(part) - Byte.MAX_VALUE -1);
+            index++;
+        }
+        return array;
+    }
 
     public long getCount(){
-        return 0;
+        //return bTree.size();
+        return set.size();
     }
 }
--- src/by/zhorau/ipaddrcout/Main.java
+++ src/by/zhorau/ipaddrcout/Main.java
@@ -1,7 +1,34 @@
 package by.zhorau.ipaddrcout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        File file = new File("res/address.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        IpAddressCounter counter = new IpAddressCounter(scanner);
+        counter.startCounting();
+        System.out.println(counter.getCount());//10
+        file = new File("res/address.txt");
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
+        Set<String> set = new HashSet<>();
+        while (scanner.hasNext()){
+            set.add(scanner.next());
+        }
+        System.out.println(set.size());
     }
 }
