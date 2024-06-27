fe14013f9592654127a44dc09db7e977601e686b
Ivan <ivanzhuraulievich@gmail.com>
2024-06-27 23:09:58 +0400

    Re-add IpAddressCounter

--- src/by/zhorau/ipaddrcount/IpAddressCounter.java
+++ src/by/zhorau/ipaddrcount/IpAddressCounter.java
@@ -1,49 +1,68 @@
 package by.zhorau.ipaddrcount;
 
-import by.zhorau.ipaddrcount.binarytree.BinaryTree;
-
 import java.nio.ByteBuffer;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 public class IpAddressCounter {
     private final Iterator<String> addressIterator;
-    private final BinaryTree binaryTree = new BinaryTree();
+    private final Set<Integer> set;
 
-    public IpAddressCounter(Iterator<String> addressIterator) {
+    public IpAddressCounter(Iterator<String> addressIterator, Set<Integer> set) {
         this.addressIterator = addressIterator;
+        this.set = set;
     }
 
     public void startCounting() {
+        long iterations = 0;
         while (addressIterator.hasNext()) {
             String address = addressIterator.next();
-            if (!isValid(address)) {
-                System.out.printf("Invalid address %s will be skipped\n", address);
+            short[] parsedAddress = convertAddressToShortArray(address);
+            if (!isValid(parsedAddress)) {
+                System.out.printf("Invalid address %s at %d line will be skipped\n", address, iterations);
                 continue;
             }
-            byte[] addressArray = convertAddressToByteArray(address);
-            int addressInt = ByteBuffer.wrap(addressArray).getInt();
-            binaryTree.add(addressInt);
+            int addressInt = convertAddressToInteger(parsedAddress);
+            set.add(addressInt);
+            iterations++;
+            if (iterations % 1000000 == 0) {
+                System.out.println("Checked " + iterations + " lines\n" + "Count " + getCount() + "\n");
+            }
         }
     }
 
-    private boolean isValid(String address) {
+    private short[] convertAddressToShortArray(String address) {
+        short[] array = new short[4];
+        byte index = 0;
         for (String part : address.split("\\.")) {
-            if (Integer.parseInt(part) > 255) return false;
-        }
-        return true;
-    }
-
-    private byte[] convertAddressToByteArray(String address) {
-        byte[] array = new byte[4];
-        int index = 0;
-        for (String part : address.split("\\.")) {
-            array[index] = (byte) (Integer.parseInt(part) - Byte.MAX_VALUE - 1);
+            array[index] = Short.parseShort(part);
             index++;
         }
         return array;
     }
 
+    private boolean isValid(short[] address) {
+        for (short part : address) {
+            if (part > 255) return false;
+        }
+        return true;
+    }
+
+    private int convertAddressToInteger(short[] address) {
+        int addressInt = 0;
+        int partInt =0;
+        byte index = 0;
+        for (short part : address) {
+            partInt = (byte)(part - Byte.MAX_VALUE - 1)<<24;
+            partInt = partInt>>>(8*index);
+            addressInt = addressInt | partInt;
+            index++;
+        }
+        return addressInt;
+    }
+
     public long getCount() {
-        return binaryTree.size();
+        return set.size();
     }
 }
